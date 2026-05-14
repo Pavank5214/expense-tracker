8dde3b7215fa3ad21a6155a6003e7dcf1ad0f196
Pavank5214 <90980965+Pavank5214@users.noreply.github.com>
2026-05-14 23:50:54 +0530

    Final Vercel Service config update Plyr

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import axios from 'axios'
 import App from './App.jsx'
 import './index.css'
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || ''
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || '/_/backend'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
